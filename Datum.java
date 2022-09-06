f431262e792037a608c9a942553093bad64affeb
FBalduin <30518880+FBalduin@users.noreply.github.com>
2022-09-06 12:39:47 +0200

    Update Datum.java

--- Datum.java
+++ Datum.java
@@ -1,10 +1,10 @@
-public class Datum {
-    int tag;
-    int monat;
-    int jahr;
+public class Date {
+    int day;
+    int month;
+    int year;
 
     // Konstruktor parsed String in Datum-Objekt
-    public Datum(String date)   {
+    public Date(String date)   {
         this.tag = Integer.parseInt(date.substring(0,2)); // Trenne Tag von '14.02.20' und wandle in Zahl um
         this.monat = Integer.parseInt(date.substring(3,5)); // s.O.
         this.jahr = Integer.parseInt(date.substring(6,8)); // s.O.
