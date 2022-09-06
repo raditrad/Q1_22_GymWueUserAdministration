8eb45ae5e1cbf9e9607030b23123534159e56c52
FBalduin <30518880+FBalduin@users.noreply.github.com>
2022-09-06 12:41:10 +0200

    Update Verwaltung.java

--- Verwaltung.java
+++ Verwaltung.java
@@ -1,12 +1,12 @@
-class Verwaltung	{
+class Administration	{
 			
 	public static void main(String[] args)	{	
 			
 			// Lege Schueler einmal an
-			Schueler u1 = new Schueler("Felix","Balduin",new Datum("14.02.89"));
-			Schueler u2 = new Schueler("Moni","Thor",new Datum("15.02.89"));
-			Schueler u3 = new Schueler("Pups","Keks",new Datum("16.02.89"));
-			Schueler u4 = new Schueler("Matt","Eagle",new Datum("17.02.89"));
+			User u1 = new User("Felix","Balduin",new Date("14.02.89"));
+			User u2 = new User("Moni","Thor",new Date("15.02.89"));
+			User u3 = new User("Pups","Keks",new Date("16.02.89"));
+			User u4 = new User("Matt","Eagle",new Date("17.02.89"));
 			
 		    // Gebe einmal beispielhaft die Daten auf der Konsole aus...
 		    u1.printData();
