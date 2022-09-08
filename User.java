effa65b094a6b84908179d8c1ca98632dee59e0d
raditrad <112942188+raditrad@users.noreply.github.com>
2022-09-08 15:15:23 +0200

    Update User.java

--- User.java
+++ User.java
@@ -18,20 +18,5 @@ public class User  {
 	System.out.println(this.lastName+", "+this.firstName+"!");
     } 
 	
-   public String getDDMM() { 
-        String dd = ""; 
-        String mm = "";
-        if(this.day<10) {
-            dd="0"+this.day;
-        } else {
-            dd=this.day+"";
-        }
-
-        if(this.month<10) {
-            mm="0"+this.month;
-        } else {
-            mm=this.month+"";
-        } 
-        return dd+mm; // verkette Tag mit Monat und gebe es zurück "0401"
-    }
+  
 }
